7f47a9026c1e1e7c597c1b9acd9ce6ddb7a0e14a
David Sanders <dsanders11@ucsbalum.com>
2023-12-16 01:15:00 -0800

    build: also lint index.ts (#11)

--- index.ts
+++ index.ts
@@ -1,28 +1,23 @@
-import "dotenv/config";
-import { Comparator } from "./src/comparator";
-import { ElectronDataSource } from "./src/dataSource";
-import { SlackReporter } from "./src/reporter";
+import 'dotenv/config';
+import { Comparator } from './src/comparator';
+import { ElectronDataSource } from './src/dataSource';
+import { SlackReporter } from './src/reporter';
 
-const {
-  SLACK_TOKEN,
-  SLACK_CHANNEL_ID,
-  POSTGRES_URI
-} = process.env;
+const { SLACK_TOKEN, SLACK_CHANNEL_ID, POSTGRES_URI } = process.env;
 
 const main = async () => {
-  const source = new ElectronDataSource(POSTGRES_URI ?? "invalid-uri");
+  const source = new ElectronDataSource(POSTGRES_URI ?? 'invalid-uri');
   const reporter = new SlackReporter(
-    SLACK_TOKEN ?? "invalid-token",
-    SLACK_CHANNEL_ID ?? "invalid-id",
+    SLACK_TOKEN ?? 'invalid-token',
+    SLACK_CHANNEL_ID ?? 'invalid-id',
   );
 
   const comparator = new Comparator(source, reporter);
 
   await comparator.compareLatestVersions();
   await source.closeConnection();
-}
+};
 
 main().catch((err) => {
   console.error(err);
-})
-
+});
